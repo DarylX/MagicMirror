edbca6487da7fd5d42db76cde3f9d8039e6d6be0
DarylX <darylXayavong@gmail.com>
2016-01-08 17:33:57 -0600

    + Added storage

---
+++ app/src/main/java/com/ppg/magicmirror/utility/UserStorage.java
@@ -0,0 +1,41 @@
+package com.ppg.magicmirror.utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Daryl on 1/8/2016.
+ */
+public class UserStorage {
+
+    static public void savePictures(Context context, ArrayList <String> urls){
+        SharedPreferences prefs;
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        for (int i = 0; i < urls.size(); i++){
+            editor.putString("userPhoto"+i,urls.get(i));
+        }
+        editor.commit();
+    }
+
+    static public ArrayList<String> getPictures(Context context){
+
+        ArrayList<String> strings = new ArrayList<>();
+        String tempString;
+
+        SharedPreferences prefs;
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        for(int i = 0; i < 5; i++){
+            tempString = prefs.getString("userPhoto"+i,null);
+            if(tempString!=null){
+                strings.add(tempString);
+            }
+        }
+
+        return strings;
+    }
+}
